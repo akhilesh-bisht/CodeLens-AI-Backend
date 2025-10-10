4fb546f2451f6e0864743357feb21e752090eba2
akhilesh <akhileshbisht2020@gmail.com>
2025-10-10 21:23:27 +0530

    create navbar v1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,16 +22,20 @@ const navLinks: navLinks[] = [
 
 export const Navbar = () => {
   return (
-    <div className="w-[1240px] mx-auto h-7 bg-green-400">
-      <h1>SkillForge</h1>
-      <div className="bg-red-600 ">
-        {navLinks.map((link) => {
-          return (
-            <ul key={link.url} className="flex justify-evenly items-center">
-              <li>{link.name}</li>
-            </ul>
-          );
-        })}
+    <div className="w-full h-full  bg-gray-700">
+      <div className="h-7 w-[1240px] mx-auto">
+        <h1>SkillForge</h1>
+        <div className="bg-white">
+          {navLinks.map((link) => {
+            return (
+              <ul key={link.url} className="flex justify-evenly items-center">
+                <li className="text-xl text-gray-400 hover:text-gray-600">
+                  {link.name}
+                </li>
+              </ul>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
